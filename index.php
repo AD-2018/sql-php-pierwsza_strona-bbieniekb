4e8d37685b1cbb0229b472c601dd903daf64445c
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2020-10-29 14:50:23 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-echo("Siema");
+$conn = new mysqli("mysql-bbieniekb.alwaysdata.net","bbieniekb","20ZAQHauswsx02","bbieniekb_baza");
+$sql = "SELECT * FROM pracownicy";
+$wynik = mysqli_query($conn, $sql);
+
+
+    echo('<table border="1">');
+    echo('<th>Imie</th><th>zarobki</th>');
+
+    while($wiersz=mysqli_fetch_assoc($wynik)){
+        echo('<tr>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
+        echo('</tr>');
+    }
+
+    echo('</table>');
+
 
 ?>
