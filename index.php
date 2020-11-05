60b2e8b4a4513e842eeaf0edc5002263b9dc75da
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2020-11-05 14:02:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,72 +13,18 @@ $sql = "SELECT * FROM pracownicy";
 $wynik = mysqli_query($conn, $sql);
 
 
-  require_once("lib.php");
-echo ("<br>Zad.1<br>");
-$sql = "SELECT * FROM pracownicy";
-echo ("<li>".$sql);
-  $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-  
-
-echo('<table border="1">');
+ 
+    echo('<table border="1">');
     echo('<th>Imie</th><th>zarobki</th>');
 
-    while($row=mysqli_fetch_assoc($result)){
+    while($wiersz=mysqli_fetch_assoc($wynik)){
         echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
+        echo('<td>'.$wiersz['imie'].'</td>'.'<td>'.$wiersz['zarobki'].'</td>');
         echo('</tr>');
     }
 
     echo('</table>');
-  
-  echo ("<br>Zad.2<br>");
-$sql = "SELECT * FROM pracownicy WHERE imie like '%a'";
-echo ("<li>".$sql);
-  $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-  
 
-echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
-  
-  echo ("<br>Zad.3<br>");
-$sql = "SELECT * FROM pracownicy WHERE imie not like '%a'";
-echo ("<li>".$sql);
-  $result = mysqli_query($conn, $sql);
-    if ( $result) {
-        echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-  
-
-echo('<table border="1">');
-    echo('<th>Imie</th><th>zarobki</th>');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>');
-        echo('</tr>');
-    }
-
-    echo('</table>');
 ?>
     </body>
 </html>
