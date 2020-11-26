6b3dfa9900fea40efd547a5e1a0f58bcd100105c
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2020-11-26 14:16:34 +0100

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,30 @@
+  
+<?php
+echo("jestes w delete.php <br>");
+echo $_POST['id'];
+
+
+$servername = "mysql-bbieniekb.alwaysdata.net";
+$username = "bbieniekb";
+$password = "dorsz1234";
+$dbname = "bbieniekb_baza";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id'];
+
+//wyświetlamy zapytanie $sql
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
