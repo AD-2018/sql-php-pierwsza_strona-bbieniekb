086509f128318567ae600a3e8dbfc6d06655185a
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2020-11-26 14:19:15 +0100

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,9 +1,15 @@
 <?php
+echo("jestes w insert.php <br>");
+echo "<li>". $_POST['imie'];
+echo "<li>". $_POST['dzial'];
+echo "<li>". $_POST['zarobki'];
+echo "<li>". $_POST['data_urodzenia'];
 
 $servername = "mysql-bbieniekb.alwaysdata.net";
 $username = "bbieniekb";
 $password = "dorsz1234";
 $dbname = "bbieniekb_baza";
+
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -11,9 +17,14 @@ if ($conn->connect_error) {
 }
 
 $sql = "INSERT INTO pracownicy (id_pracownicy, imie, dzial, zarobki, data_urodzenia) 
-       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+       VALUES (null, '".$_POST['imie']."', '".$_POST['dzial']."','".$_POST['zarobki']."','".$_POST['data_urodzenia']."')";
 
-$conn->query($sql);
+if ($conn->query($sql) === TRUE) {
+  echo "<br>New record created successfully";
+} else {
+//informacja o ewentualnych błędach
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 $conn->close();
 ?>
