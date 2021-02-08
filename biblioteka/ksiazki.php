824a81d7b1fd5365d1394c6d06959473055f38c7
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2021-02-08 10:29:33 +0100

    l

--- biblioteka/ksiazki.php
+++ biblioteka/ksiazki.php
@@ -28,17 +28,16 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 <?php
-   require_once("lib.php");
-
 echo ("<br>Autorzy<br>");
 $sql = "SELECT * FROM bibl_autor";
+echo ("<li>".$sql);
   $result = mysqli_query($conn, $sql);
     if ( $result) {
-        
+        echo "<li>ok";
     } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
-
+  
 
 echo('<select name="Autor">');
 
@@ -48,20 +47,20 @@ echo('<select name="Autor">');
         echo"</option>"; 
     }
 echo('</select>');
-
-echo("<br>");
-
-echo ("<br>Tytuły<br>");
+  echo("<br>");
+  
+  echo ("<br>Tytuły<br>");
 $sql = "SELECT * FROM bibl_tytul";
+echo ("<li>".$sql);
   $result = mysqli_query($conn, $sql);
     if ( $result) {
         echo "<li>ok";
     } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
+  
 
-
-echo('<select name="Tytul">');
+echo('<select name="Tytuł">');
 
     while($row=mysqli_fetch_assoc($result)){
         echo'<option value="'.$row['id_tytul'].'">';
@@ -69,20 +68,21 @@ echo('<select name="Tytul">');
         echo"</option>"; 
     }
 echo('</select>');
-
-echo("<br>");
-echo("<br>KSIĄŻKI I AUTORZY<br>");
-$sql = "SELECT id_book, autor, tytul FROM bibl_book, bibl_tytul, bibl_autor WHERE bibl_tytul.id_tytul = bibl_book.id_tytul AND bibl_autor.id_autor = bibl_book.id_autor";
-echo($sql);
-
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-           } else {
+  echo("<br>");
+  
+  echo ("<br>Książki<br>");
+$sql = "SELECT id_book, autor, tytul FROM bibl_book, bibl_tytul, bibl_autor WHERE bibl_tytul.id_tytul = bibl_book.id_tytul AND bibl_autor.id_autor = bibl_book.id_autor order by id_book asc";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
+  
 
 echo('<table border="1">');
-    echo('<th>ID BOOK</th><th>ID AUTOR</th><th>ID TYTUL</th>');
+    echo('<th>Id</th><th>Autor</th><th>Tytuł</th>');
 
     while($row=mysqli_fetch_assoc($result)){
         echo('<tr>');
@@ -90,6 +90,8 @@ echo('<table border="1">');
         echo('</tr>');
     }
 
-echo('</table>');
-
-?>
+    echo('</table>');
+    ?>
+      </div>
+    </body>
+    </html>
