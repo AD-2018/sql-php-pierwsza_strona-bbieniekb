ec572b48d8288e08fadbc4b882d9ba8443fe7891
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2021-04-15 10:51:32 +0200

    a

---
+++ gridcss/3zadanie/3strona.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css">
+    <title>Grid</title>
+  </head>
+  <body>
+    <div class="container">
+      <header>
+      <div class="tabela1">
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM mechanicy";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>id</th><th>mechanik</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['mechanik'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
+        </div>
+      </header>
+      <nav>
+      2
+      </nav>
+      <main>
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT warsztat.id, mechanik, rejestracja FROM mechanicy, warsztat, rejestracje where mechanicy.id=warsztat.mechanik_id and rejestracje.id=warsztat.rejestracja_id";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>id</th><th>mechanik</th><th>Uczeń</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['rejestracja'].'</td><td>'.$row['mechanik'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
+      </main>
+      <footer>
+      4
+      </footer>
+      <aside>
+      <div class="tabela1">
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM rejestracje";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id</th><th>rejestracja</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['rejestracja'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
+        </div>
+      </aside>
+    </div>
+  </body>
+</html>
