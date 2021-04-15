197b8ae07f2920b9a72180f0c996892823bde792
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2021-04-15 09:23:10 +0200

    a

--- gridcss/1zadanie/1strona.php
+++ gridcss/1zadanie/1strona.php
@@ -4,53 +4,87 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Bartosz Bieniek</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="style.css">
+    <title>Grid</title>
   </head>
   <body>
     <div class="container">
-    <div class="header">
+      <header>
+      <div class="tabela1">
       <?php
-require_once("../../lib.php");
-
-echo("<br>nauczyciele<br>");
-$sql = "SELECT * FROM nauczyciele";
-echo($sql);
-
-$result = mysqli_query($conn, $sql);
-if ( $result) {
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM nauczyciele";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
         echo "<li>ok";
-    } else {
-      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-
-echo('<table border="1">');
-    echo('<th>ID</th><th>nauczyciele</th>');
-
-    while($row=mysqli_fetch_assoc($result)){
-        echo('<tr>');
-        echo('<td>'.$row['id'].'</td><td>'.$row['nauczyciele'].'</td>');
-        echo('</tr>');
-    }
-
-echo('</table>');
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>id</th><th>Nauczyciel</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['Nauczyciel'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
 ?>
-</div>
-    <header>
-        <h1>gridcss</h1>
+        </div>
       </header>
       <nav>
-        2
+      2
       </nav>
       <main>
-        3
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT id, Nauczyciel, nazwisko FROM nauczyciele, szkola, Uczen where nauczyciele.id=szkola.nauczyciele_id and Uczen.id=szkola.uczen_id";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id</th><th>Nauczyciel</th><th>Uczeń</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['nazwisko'].'</td><td>'.$row['Nauczyciel'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
       </main>
-      <aside>
-        5
-      </aside>
       <footer>
-        4
+      4
       </footer>
+      <aside>
+      <div class="tabela1">
+      <?php
+    require_once("../../connect.php");
+    $sql = "SELECT * FROM Firma_projekt";
+    echo("<br>");
+    echo($sql);
+    $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+        } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+     }
+    echo('<table border="1">');
+        echo('<th>Id</th><th>Uczen</th>');
+        while($row=mysqli_fetch_assoc($result)){
+            echo('<tr>');
+            echo('<td>'.$row['id'].'</td><td>'.$row['Uczen'].'</td>');
+            echo('</tr>');
+     }
+        echo('</table>');
+?>
+        </div>
+      </aside>
     </div>
   </body>
 </html>
