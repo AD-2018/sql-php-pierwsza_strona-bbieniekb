a991b896fc7d0d161b18d6d03819168a1cee0c5e
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2021-04-15 08:51:01 +0200

    a

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "mysql-bbieniekb.alwaysdata.net";
+$username = "bbieniekb";
+$password = "dorsz1234";
+$dbname = "bbieniekb_baza";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
