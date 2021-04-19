4585b5ee03c176a5f918d29d48d51d51d13e158c
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2021-04-19 10:09:22 +0200

    a

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
     <link rel="stylesheet" href="/style.css">
     
-    <div class="buttons">
+    <div class="menu">
         <ul>
     <li><a class="btn effect01" href="/index.php">Strona Główna</a></li>
 
