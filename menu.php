e3ba8e438a428ea259f2d8ee40a149c1a106de20
BieniekBartosz <63042206+BieniekBartosz@users.noreply.github.com>
2021-04-19 09:43:18 +0200

    a

--- menu.php
+++ menu.php
@@ -1,96 +1,32 @@
-<link rel="stylesheet" href="style.css">
-<div class="menu">
-        <br/>
-    <br/>
-    <div class="buttons">
-      <a class="btn effect01" href="/index.php">Strona Główna</a></br>
-        <br/>
-        <br/>
+    <link rel="stylesheet" href="style.css">
+    
+        <div>
+        <ul>
+    <li><a class="btn effect01" href="/index.php">Strona Główna</a></li>
 
-        <br/>
-        <br/>
-        <div class="buttons">
-          <a class="btn effect01" href="/gridcss/1zadanie/1strona.php">Strona 1</a></br>
-            <br/>
-            <br/>
+    <li><a class="btn effect01" href="/gridcss/1zadanie/1strona.php">Strona 1</a></li>
+        
+    <li><a class="btn effect01" href="/gridcss/2zadanie/2strona.php">Strona 2</a></li>
 
-            <br/>
-            <br/>
-            <div class="buttons">
-              <a class="btn effect01" href="/gridcss/2zadanie/2strona.php">Strona 2</a></br>
-                <br/>
-                <br/>
+    <li><a class="btn effect01" href="/gridcss/3zadanie/3strona.php">Strona 3</a></li>
 
-                <br/>
-                <br/>
-                <div class="buttons">
-                  <a class="btn effect01" href="/gridcss/3zadanie/3strona.php">Strona 3</a></br>
-                    <br/>
-                    <br/>
+    <li><a class="btn effect01" href="/gridcss/4zadanie/4strona.php">Strona 4</a></li>
 
-                    <br/>
-                    <br/>
-                    <div class="buttons">
-                      <a class="btn effect01" href="/gridcss/4zadanie/4strona.php">Strona 4</a></br>
-                        <br/>
-                        <br/>
-   
-                        <br/>
-                        <br/>
-                        <div class="buttons">
-                          <a class="btn effect01" href="/gridcss/5zadanie/5strona.php">Strona 5</a></br>
-                            <br/>
-                            <br/>
+    <li><a class="btn effect01" href="/gridcss/5zadanie/5strona.php">Strona 5</a></li>
 
-                            <br/>
-                            <br/>
-                            <div class="buttons">
-                              <a class="btn effect01" href="/gridcss/6zadanie/6strona.php">Strona 6</a></br>
-                                <br/>
-                                <br/>
+    <li><a class="btn effect01" href="/gridcss/6zadanie/6strona.php">Strona 6</a></li>
 
-                                <br/>
-                                <br/>
-                                <div class="buttons">
-                                  <a class="btn effect01" href="/gridcss/7zadanie/7strona.php">Strona 7</a></br>
-                                    <br/>
-                                    <br/>       
-                                    
-                                    <br/>
-                            <br/>
-                            <div class="buttons">
-                              <a class="btn effect01" href="/gridcss/8zadanie/8strona.php">Strona 8</a></br>
-                                <br/>
-                                <br/>
+    <li><a class="btn effect01" href="/gridcss/7zadanie/7strona.php">Strona 7</a></li>
 
-                                <br/>
-                            <br/>
-                            <div class="buttons">
-                              <a class="btn effect01" href="/gridcss/9zadanie/9strona.php">Strona 9</a></br>
-                                <br/>
-                                <br/>
+    <li><a class="btn effect01" href="/gridcss/8zadanie/8strona.php">Strona 8</a></li>
 
-                                <br/>
-                                <br/>
-                                <div class="buttons">
-                                  <a class="btn effect01" href="/gridcss/10zadanie/10strona.php">Strona 10</a></br>
-                                    <br/>
-                                    <br/>
+    <li><a class="btn effect01" href="/gridcss/9zadanie/9strona.php">Strona 9</a></li>
 
-                                    <br/>
-                                    <br/>
-                                    <div class="buttons">
-                                      <a class="btn effect01" href="/gridcss/11zadanie/11strona.php">Strona 11</a></br>
-                                        <br/>
-                                        <br/>
+    <li><a class="btn effect01" href="/gridcss/10zadanie/10strona.php">Strona 10</a></li>
 
-                                        <br/>
-                                        <br/>
-                                        <div class="buttons">
-                                          <a class="btn effect01" href="/gridcss/12zadanie/12strona.php">Strona 12</a></br>
-                                            <br/>
-                                            <br/>
+    <li><a class="btn effect01" href="/gridcss/11zadanie/11strona.php">Strona 11</a></li>
 
-
-      </div>
-  </body>
+    <li><a class="btn effect01" href="/gridcss/12zadanie/12strona.php">Strona 12</a></li>
+        </ul>
+    </div>
+    </body>
